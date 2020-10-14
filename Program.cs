9b639be5415ce6dbfa4cd367e642aae2dc97d40a
Frederick Javalera <frederickjavalera@gmail.com>
2020-10-13 21:41:38 -0600

    before state

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExampleWithoutUsingDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+
+        private static void Calculate()
+        {
+            var left = int.Parse(Console.ReadLine());
+            var right = int.Parse(Console.ReadLine());
+
+            var sum = Sum(left, right);
+
+            Print(sum);
+        }
+
+        private static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Product(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static void Print(int result)
+        {
+            System.Console.WriteLine($"Result is {result}.");
+        }
+    }
+}
